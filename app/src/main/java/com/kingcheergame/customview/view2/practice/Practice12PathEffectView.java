b68a9154c8f29e088e890908eae9855b6279f04f
won <wenwenwen888@live.com>
2018-08-20 16:10:18 +0800

    Complete HenCoder View 2

--- app/src/main/java/com/kingcheergame/customview/view2/practice/Practice12PathEffectView.java
+++ app/src/main/java/com/kingcheergame/customview/view2/practice/Practice12PathEffectView.java
@@ -1,9 +1,17 @@
 package com.kingcheergame.customview.view2.practice;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -35,42 +43,55 @@ public class Practice12PathEffectView extends View {
         path.rLineTo(150, 80);
     }
 
+    @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
-        // 第一处：CornerPathEffect
+        // 第一处：CornerPathEffect , 把所有拐角变成圆角
+        paint.setPathEffect(new CornerPathEffect(20));
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
-        // 第二处：DiscretePathEffect
+        // 第二处：DiscretePathEffect , 把线条进行随机的偏离，让轮廓变得乱七八糟
+        paint.setPathEffect(new DiscretePathEffect(20, 5));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
-        // 第三处：DashPathEffect
+        // 第三处：DashPathEffect , 虚线来绘制线条
+        paint.setPathEffect(new DashPathEffect(new float[]{20, 10, 5, 10}, 0));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 200);
-        // 第四处：PathDashPathEffect
+        // 第四处：PathDashPathEffect , 使用一个 Path 来绘制「虚线」
+        Path dashPath = new Path();
+        dashPath.rLineTo(20, -20);
+        dashPath.rLineTo(20, 20);
+        dashPath.close();
+        paint.setPathEffect(new PathDashPathEffect(dashPath, 40, 0, PathDashPathEffect.Style.TRANSLATE));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
-        // 第五处：SumPathEffect
+        // 第五处：SumPathEffect , 组合效果类的 PathEffect
+        PathEffect pathEffect = new DashPathEffect(new float[]{20, 10, 5, 10}, 0);
+        PathEffect discreteEffect = new DiscretePathEffect(20, 5);
+        paint.setPathEffect(new SumPathEffect(pathEffect, discreteEffect));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
-        // 第六处：ComposePathEffect
+        // 第六处：ComposePathEffect , 组合效果类的 PathEffect
+        paint.setPathEffect(new ComposePathEffect(pathEffect, discreteEffect));
         canvas.drawPath(path, paint);
         canvas.restore();
     }
--- app/src/main/java/com/kingcheergame/customview/view2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/kingcheergame/customview/view2/practice/Practice14MaskFilterView.java
@@ -1,8 +1,10 @@
 package com.kingcheergame.customview.view2.practice;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
@@ -32,22 +34,28 @@ public class Practice14MaskFilterView extends View {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.what_the_fuck);
     }
 
+    @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
+        // 设置的是在绘制层上方的附加效果 , 模糊效果的 MaskFilter。
 
-        // 第一个：NORMAL
+        // 第一个：NORMAL 内外都模糊绘制
+        paint.setMaskFilter(new BlurMaskFilter(50, BlurMaskFilter.Blur.NORMAL));
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
-        // 第二个：INNER
+        // 第二个：INNER 内部模糊，外部不绘制
+        paint.setMaskFilter(new BlurMaskFilter(50, BlurMaskFilter.Blur.INNER));
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
-        // 第三个：OUTER
+        // 第三个：OUTER 内部不绘制，外部模糊
+        paint.setMaskFilter(new BlurMaskFilter(50, BlurMaskFilter.Blur.OUTER));
         canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
-        // 第四个：SOLID
+        // 第四个：SOLID 内部正常绘制，外部模糊
+        paint.setMaskFilter(new BlurMaskFilter(50, BlurMaskFilter.Blur.SOLID));
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
